64a4e395418fca828c276f74e2b7a8f622932b94
jorditm <jorditorrasm@gmail.com>
2024-05-20 12:00:02 +0200

    style card

--- frontend/components/searcher.tsx
+++ frontend/components/searcher.tsx
@@ -33,13 +33,24 @@ const Searcher = () => {
                     onChange={(e) => handleChange(e)}
                 />
             </div>
-            <div className="grid grid-cols-4 gap-8">
+            <div className="grid grid-cols-3 gap-8">
                 {filteredList().map((item) => (
-                    <div className="text-white mx-auto space-y-4">
-                        <div className="w-24 h-24 mx-auto rounded-full border border-primary border-white bg-primary"></div>
-                        <div className="flex flex-col gap-2">
-                            <p>{item.name}</p>
-                            <p>{item.year}</p>
+                    <div className=" group w-full h-full p-6 text-white mx-auto space-y-4 rounded-md transition duration-300 border border-transparent hover:border-primary">
+                        <div className="w-30 h-30 mx-auto rounded-full border border-primary border-white bg-[#ee96f5]">
+                            <img src="./vaultium-logo.png" />
+                        </div>
+                        <div className="flex flex-col gap-6">
+                            <p className="text-xl group-hover:text-primary transition-colors duration-300">{item.name}</p>
+                            <div className="flex flex-col">
+                                <div className="flex justify-between">
+                                    <p>{item.platform}</p>
+                                    <p>{item.year}</p>
+                                </div>
+                                <p>{item.releasedIn}</p>
+                                <div className="truncate">
+                                    {item.theme.map((element) => <span>{element}, </span>)}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ))}
