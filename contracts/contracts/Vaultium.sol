8c17c3d9652fc52d3fa4e5cc69ea7240e3cb8c53
jorvixsky <jorvixsky@proton.me>
2024-05-20 18:09:30 +0200

    chore: Refactor code structure and improve game search functionality

--- contracts/contracts/Vaultium.sol
+++ contracts/contracts/Vaultium.sol
@@ -47,23 +47,92 @@ contract Vaultium {
 
     struct GameChallengeHistory {
         Challenge[] challenge;
-    } 
+    }
 
     mapping(bytes32 => GameInfo) public game;
     mapping(bytes32 => Challenge) public challenge;
 
-    function getGameHash(string memory name, uint16 year, string memory publisher) private pure returns (bytes32) {
-        return keccak256(abi.encodePacked(name, year, publisher));
+    function getGameHash(
+        string memory _name,
+        uint16 _year,
+        string memory _publisher
+    ) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_name, _year, _publisher));
+    }
+
+    function validateGameInfo(
+        GameInfo memory _gameInfo
+    ) private pure returns (bool) {
+        if (
+            !(_gameInfo.year > 1822) ||
+            bytes(_gameInfo.name).length == 0 ||
+            bytes(_gameInfo.publisher).length == 0
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    function fillGameInfo(
+        GameInfo memory _gameInfo
+    ) private pure returns (GameInfo[] memory) {
+        GameInfo[] memory gameInfo = new GameInfo[](1);
+        gameInfo[0] = _gameInfo;
+        
+        return filterValidGames(gameInfo);
+    }
+
+    /**
+     * @dev Given an array of games, it returns a new array with only the valid games
+     * @param gameInfo An array of GameInfo structs
+     * @return GameInfo An array of GameInfo structs with only the valid games
+     */
+
+    function filterValidGames(
+        GameInfo[] memory gameInfo
+    ) private pure returns (GameInfo[] memory) {
+        bool[] memory validGames = new bool[](gameInfo.length);
+        uint256 validGamesCount = 0;
+        for (uint256 i = 0; i < gameInfo.length; i++) {
+            validGames[i] = validateGameInfo(gameInfo[i]);
+            if (validGames[i]) {
+                validGamesCount++;
+            }
+        }
+        GameInfo[] memory validGamesResult = new GameInfo[](validGamesCount);
+        uint256 validGamesIndex = 0;
+        for (uint256 i = 0; i < gameInfo.length; i++) {
+            if (validGames[i]) {
+                validGamesResult[validGamesIndex] = gameInfo[i];
+                validGamesIndex++;
+            }
+        }
+        return validGamesResult;
     }
 
     // TODO: Maybe change function name? It is not only searching but inserting if not found
-    function searchAbandonware(string memory name, string memory description, string memory publisher, uint16 year) public returns (GameInfo memory) { 
-        bytes32 gameHash = getGameHash(name, year, publisher);
-        GameInfo memory searchedGame = game[gameHash];
-        if (searchedGame.year == 0) {
-            game[gameHash] = GameInfo(name, year, publisher, description, true); // TODO: Check if it is abandonware
+    function searchAbandonware(
+        string memory _name,
+        string memory _description,
+        string memory _publisher,
+        uint16 _year
+    ) public returns (GameInfo[] memory) {
+        GameInfo[] memory gameInfo = fillGameInfo(
+            GameInfo(_name, _year, _publisher, _description, true)
+        );
+        require(gameInfo.length > 0, "No valid games found.");
+        for (uint256 i = 0; i < gameInfo.length; i++) {
+            bytes32 gameHash = getGameHash(
+                gameInfo[i].name,
+                gameInfo[i].year,
+                gameInfo[i].publisher
+            );
+            if (game[gameHash].year == 0) {
+                game[gameHash] = gameInfo[i];
+            } else {
+                gameInfo[i] = game[gameHash];
+            }
         }
-        return game[gameHash];
+        return gameInfo;
     }
-
 }
